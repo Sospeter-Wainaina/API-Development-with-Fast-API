5b5bff4c8fee3a4f7cdef9bd77f3c3a4a150188c
sospeter-wainaina <sospetergathungu2000@gmail.com>
2023-04-10 10:48:30 +0300

    Delete Post request

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ def find_post(id):
             return p
 
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 def root():
     return {"message": "Hello Sospeter Wainaina Gathungu"}
@@ -56,3 +62,14 @@ def get_post(id: int, response: Response):
         # return {"message": f"Post id {id} Cannot be found"}
     print(id)
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # deleting a post
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with Id {id} does not Exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
